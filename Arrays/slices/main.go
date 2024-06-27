f80f819dafd6b85248ab04a6cfa0894fc9217275
Ayush Kumar <ayush.krsh9@gmail.com>
2024-06-27 15:21:12 +0530

    feat: added slices

--- Arrays/slices/main.go
+++ Arrays/slices/main.go
@@ -6,4 +6,24 @@ import(
 
 func main(){
 	fmt.Println("slices")
+	s:= []int{1,2,3,4,5,6,7,8,9}
+
+	fmt.Println(s);
+
+	fmt.Println(len(s))
+	fmt.Println(cap(s))
+
+	t:= s[1:4]
+	fmt.Println(t)
+
+	s= append(s, 69,35)
+	fmt.Println(s)
+
+	u:=make([]int, len(s))
+	copy(u,s)
+	fmt.Println(u)
+
+	for i,v :=range u{
+		fmt.Println(i," ",v)
+	}
 }
