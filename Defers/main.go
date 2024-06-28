fed9d1c12f4b254dd6a7e33c063775ec6fd10dbc
Ayush Kumar <ayush.krsh9@gmail.com>
2024-06-28 09:42:50 +0530

    feat: added defer

--- Defers/main.go
+++ Defers/main.go
@@ -4,10 +4,21 @@ import (
 	"fmt"
 )
 
-func main(){
+func main() {
 	fmt.Println("Process Started")
 	defer fmt.Println("first defer")
 	defer fmt.Println("second defer")
 	fmt.Println("execution finshed")
-	
+
+	for i := 1; i <= 5; i++ {
+		defer fmt.Println(i)
+	}
+	fmt.Println("loop finsihed")
+	/**
+		5
+		4
+		3
+		2
+		1
+	**/
 }
